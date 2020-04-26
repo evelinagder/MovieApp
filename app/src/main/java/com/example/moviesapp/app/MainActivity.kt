f98e3d98dd3c71074fe07bf1cda6f13e15fe8458
ederventska <uBr+WRoQ1dr&v>
2020-04-26 14:36:40 +0300

    bottom tab in xml, small design imporvements

--- app/src/main/java/com/example/moviesapp/app/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/app/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.moviesapp.app
 
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.moviesapp.R
+import androidx.navigation.ui.NavigationUI
 import com.example.moviesapp.BR
+import com.example.moviesapp.R
 import com.example.moviesapp.view.BaseActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity :
     BaseActivity<com.example.moviesapp.databinding.ActivityMainBinding, MainActivityViewModel>() {
 
     override fun getViewModelResId(): Int = BR.mainActivityVM
 
-    override fun getViewModelClass(): Class<MainActivityViewModel> = MainActivityViewModel::class.java
+    override fun getViewModelClass(): Class<MainActivityViewModel> =
+        MainActivityViewModel::class.java
 
     override fun getLayoutResId(): Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpNavigation()
+    }
 
+   private fun setUpNavigation() {
+        val bottomNavigationView = findViewById<View>(R.id.bottom_nav) as BottomNavigationView
+        NavigationUI.setupWithNavController(
+            bottomNavigationView,
+            findNavController(R.id.nav_host_fragment)
+        )
     }
 }
