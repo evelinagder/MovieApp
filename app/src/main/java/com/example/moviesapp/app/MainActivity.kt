2a8c5d4bf82904bd3d7d10ee8eba3fa52cfd9406
raya.ivanova <ggYTJR7Q!>
2020-04-28 17:20:46 +0300

    Handle bottom navigation and loading the app modules dynamically

--- app/src/main/java/com/example/moviesapp/app/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.moviesapp.app
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviesapp.BR
@@ -11,28 +10,29 @@ import com.example.moviesapp.R
 import com.example.moviesapp.view.BaseActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class MainActivity :
-    BaseActivity<com.example.moviesapp.databinding.ActivityMainBinding, MainActivityViewModel>() {
+	BaseActivity<com.example.moviesapp.databinding.ActivityMainBinding, MainActivityViewModel>() {
 
-    override fun getViewModelResId(): Int = BR.mainActivityVM
+	override fun getViewModelResId(): Int = BR.mainActivityVM
 
-    override fun getViewModelClass(): Class<MainActivityViewModel> =
-        MainActivityViewModel::class.java
+	override fun getViewModelClass(): Class<MainActivityViewModel> =
+		MainActivityViewModel::class.java
 
-    override fun getLayoutResId(): Int = R.layout.activity_main
+	override fun getLayoutResId(): Int = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setUpNavigation()
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setUpNavigation()
+	}
 
-   private fun setUpNavigation() {
-        val bottomNavigationView = findViewById<View>(R.id.bottom_nav) as BottomNavigationView
-       bottomNavigationView.setupWithNavController( findNavController(R.id.nav_host_fragment))
-        NavigationUI.setupWithNavController(
-            bottomNavigationView,
-            findNavController(R.id.nav_host_fragment)
-        )
-    }
+	//TODO change live tv icon with appropriate one and
+	// monitor and handle the installation progress states + customisation
+	private fun setUpNavigation() {
+		val bottomNavigationView = findViewById<View>(R.id.bottom_nav) as BottomNavigationView
+		bottomNavigationView.setupWithNavController(findNavController(R.id.nav_host_fragment))
+		NavigationUI.setupWithNavController(
+			bottomNavigationView,
+			findNavController(R.id.nav_host_fragment)
+		)
+	}
 }
