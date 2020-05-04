553e9ccd07955fea9665b3292b318dc5a92809fb
raya.ivanova <ggYTJR7Q!>
2020-05-04 15:38:48 +0300

    setting up the Navigation Drawer and show/hide the Action bar dynamically

---
+++ app/src/main/java/com/example/moviesapp/app/AboutUsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.moviesapp.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moviesapp.R
+
+class AboutUsFragment : Fragment(){
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.fragment_about_us, container, false)
+	}
+}
--- app/src/main/java/com/example/moviesapp/app/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/app/MainActivity.kt
@@ -5,9 +5,14 @@ import android.view.View
 import androidx.activity.viewModels
 import androidx.annotation.IdRes
 import androidx.lifecycle.Observer
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviesapp.BR
 import com.example.moviesapp.R
@@ -19,7 +24,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity :
     BaseActivity<com.example.moviesapp.databinding.ActivityMainBinding, MainActivityViewModel>() {
 
-    override fun getViewModelResId(): Int = BR.mainActivityVM
+	private lateinit var navController: NavController
+
+	override fun getViewModelResId(): Int = BR.mainActivityVM
 
     override fun getViewModelClass(): Class<MainActivityViewModel> =
         MainActivityViewModel::class.java
@@ -28,6 +35,7 @@ class MainActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+		navController = findNavController(R.id.nav_host_fragment)
         setUpNavigation()
 
         //listen for registration done and show bottom navigation
@@ -37,26 +45,51 @@ class MainActivity :
                 bottom_nav.visibility = View.VISIBLE
                 //This way we don`t go to registration start destination when we select Home tab
                 (nav_host_fragment as NavHostFragment).changeStartDestination(R.id.homeFragment)
+				supportActionBar?.show()
+				connectNavViewWithNavController()
             }
         })
 
         //check if the user is registered and if not change the destination dynamically
         if (!viewModel.isUserCompletedRegistration) {
+			supportActionBar?.hide()
             (nav_host_fragment as NavHostFragment).changeStartDestination(R.id.navigation_reg)
             bottom_nav.visibility = View.GONE
         }
     }
 
-    //TODO change live tv icon with appropriate one and
-    // monitor and handle the installation progress states + customisation
-    private fun setUpNavigation() {
-        val bottomNavigationView = bottom_nav as BottomNavigationView
-        bottomNavigationView.setupWithNavController(findNavController(R.id.nav_host_fragment))
-        NavigationUI.setupWithNavController(
-            bottomNavigationView,
-            findNavController(R.id.nav_host_fragment)
-        )
-    }
+	override fun onSupportNavigateUp(): Boolean {
+		return navigateUp(navController, binding.drawerLayout)
+	}
+
+	override fun onBackPressed() {
+		if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+			binding.drawerLayout.closeDrawer(GravityCompat.START)
+		} else {
+			super.onBackPressed()
+		}
+	}
+
+	private fun connectNavViewWithNavController() {
+
+		val drawerLayout = binding.drawerLayout
+
+		val appBarConfiguration = AppBarConfiguration.Builder(navController.graph).setDrawerLayout(drawerLayout).build()
+		setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+		binding.navView.setupWithNavController(navController)
+	}
+
+	//TODO change live tv icon with appropriate one and
+	// monitor and handle the installation progress states + customisation
+	private fun setUpNavigation() {
+		val bottomNavigationView = findViewById<View>(R.id.bottom_nav) as BottomNavigationView
+		bottomNavigationView.setupWithNavController(navController)
+		NavigationUI.setupWithNavController(
+			bottomNavigationView,
+			navController
+		)
+	}
 
     /**
      * This ext functions changes the start Destination dynamically
