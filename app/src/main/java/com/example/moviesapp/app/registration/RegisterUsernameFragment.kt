8d2508b571648bed8590affc2788e422f3ec8020
ederventska <uBr+WRoQ1dr&v>
2020-05-19 14:07:05 +0300

    removed import

--- app/src/main/java/com/example/moviesapp/app/registration/RegisterUsernameFragment.kt
+++ app/src/main/java/com/example/moviesapp/app/registration/RegisterUsernameFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.navigation.navGraphViewModels
@@ -47,7 +46,7 @@ class RegisterUsernameFragment : Fragment() {
                 ).show()
                 return@setOnClickListener
             }
-            viewModel.addUserNamePassword(
+            viewModel.addUserName(
                 username
             )
         }
